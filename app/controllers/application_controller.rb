35e9f4a7f9eaca644623006f1156801bd135c7b5
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-01-16 17:44:47 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,17 +11,29 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-
+    login = {:username => params[:username], :password=> params[:password]}
+    @user = User.find_by(login)
+    puts @user.to_s
+    if !@user.nil?
+      session[:user_id] = @user.id
+      redirect to '/account'
+    else
+      erb :error
+    end
   end
 
   get '/account' do
-
+    if !Helpers.is_logged_in?(session)
+      erb  :error
+    else
+      erb :account
+    end
   end
 
   get '/logout' do
-
+    session.clear
+    redirect '/'
   end
 
 
 end
-
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
+class User < ActiveRecord::Base
+  validates_presence_of  :username, :password, :balance
 
-
+  def to_s
+    "{id: #{id}, username: #{username}, password: #{password}, balance: #{balance}}"
+  end
+end
